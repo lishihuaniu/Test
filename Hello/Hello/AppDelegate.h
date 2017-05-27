7cb52f856dc2e5572f1c3c82eb4869f4a8202e05
lishihua <lishihua@100tal.com>
2017-05-27 19:59:17 +0800

    firstCommit

---
+++ Hello/Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by 李世华 on 2017/5/27.
+//  Copyright © 2017年 zhikang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by 李世华 on 2017/5/27.
+//  Copyright © 2017年 zhikang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
